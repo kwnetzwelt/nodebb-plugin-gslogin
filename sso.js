e96052a7464a4ff23e935b06646b1797470a9cd6
markcarey <mark@markcarey.com>
2015-09-10 09:30:37 -0400

    test jquery login POST

--- sso.js
+++ sso.js
@@ -4,4 +4,15 @@ if (app.user.uid) {
   console.log("logged in!");
 } else {
   console.log("not logged in!");
+  var sessionId = 1;
+  if (sessionId)
+    $.ajax({
+      type: "POST",
+      url: "/login",
+      data: "username=session&password=session&remember=1&returnTo=" . document.URL,
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  }
 }
