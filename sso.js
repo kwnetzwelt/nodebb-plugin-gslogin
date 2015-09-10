58556ba54a870f74415aed06568f67a0ebaf51ad
markcarey <mark@markcarey.com>
2015-09-10 10:31:05 -0400

    enclosed in docready block

--- sso.js
+++ sso.js
@@ -1,23 +1,25 @@
-console.log("client js");
+$(document).ready(function() {
 
-if (app.user.uid) {
-  console.log("logged in!");
-} else {
+    if (app.user.uid) {
+      console.log("logged in!");
+    } else {
   
-  console.log("not logged in!");
-  var sessionId = Cookies.get('connect.sid');
-  if (sessionId) {
-      console.log(sessionID);
-			$.ajax('/login', {
-				type: 'POST',
-				data: "username=session&password=session&remember=1&returnTo=" + document.URL,
-				headers: {
-					'x-csrf-token': config.csrf_token
-				},
-				success: function() {
-					window.location.href = document.URL;
-				}
-			});
+      console.log("not logged in!");
+      var sessionId = Cookies.get('connect.sid');
+      if (sessionId) {
+          console.log(sessionID);
+    			$.ajax('/login', {
+    				type: 'POST',
+    				data: "username=session&password=session&remember=1&returnTo=" + document.URL,
+    				headers: {
+    					'x-csrf-token': config.csrf_token
+    				},
+    				success: function() {
+    					window.location.href = document.URL;
+    				}
+    			});
 
-  }
-}
+      }
+    }
+    
+});
