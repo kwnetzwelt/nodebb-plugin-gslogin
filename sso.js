5a10ff3b7a2c133835ab0a37d2ac848bee98a530
markcarey <mark@markcarey.com>
2015-09-10 10:04:02 -0400

    another approach for csrf

--- sso.js
+++ sso.js
@@ -7,19 +7,20 @@ if (app.user.uid) {
   console.log("not logged in!");
   var sessionId = 1;
   if (sessionId) {
-    require(['csrf'], function(csrf) {
-      console.log('inside require block');
+    //require(['csrf'], function(csrf) {
+      //console.log('inside require block');
+      console.log(config);
 			$.ajax('/login', {
 				type: 'POST',
 				data: "username=session&password=session&remember=1&returnTo=" + document.URL,
 				headers: {
-					'x-csrf-token': csrf.get()
+					'x-csrf-token': config.csrf_token
 				},
 				success: function() {
 					window.location.href = document.URL;
 				}
 			});
-    });
+    //});
     //$.ajax({
     //  type: "POST",
     //  url: "/login",
