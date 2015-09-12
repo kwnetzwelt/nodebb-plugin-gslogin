ceffb900ec245424c7cb68966956b11595fd7d4e
markcarey <mark@markcarey.com>
2015-09-12 09:08:59 -0400

    point to dev app to test cors

--- sso.js
+++ sso.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
       console.log("logged in!");
     } else {
       
-      $.get( "https://app.contentblvd.com/getCurrent", function( data ) {
+      $.get( "https://products.contentblvd.com:4000/getCurrent", function( data ) {
         console.debug(data);
       });
   
