320bebd079b40e6e080ac88def780add42fee2ac
markcarey <mark@markcarey.com>
2015-09-28 22:31:44 -0400

    bearer strategy approach

--- library.js
+++ library.js
@@ -1,59 +1,175 @@
-var winston = require('winston');
-var cookie = require('cookie-signature');
-var request = require('request');
+(function(module) {
+	"use strict";
 
-var	passport = module.parent.require('passport'),
-	passportLocal = module.parent.require('passport-local').Strategy,
-	plugin = {};
+	/*
+		Welcome to the SSO OAuth plugin! If you're inspecting this code, you're probably looking to
+		hook up NodeBB with your existing OAuth endpoint.
 
-plugin.login = function() {
-	winston.info('[login] Registering new local login strategy');
-	passport.use(new passportLocal({passReqToCallback: true}, plugin.continueLogin));
-};
+		Step 1: Fill in the "constants" section below with the requisite informaton. Either the "oauth"
+				or "oauth2" section needs to be filled, depending on what you set "type" to.
 
-plugin.continueLogin = function(req, username, password, next) {
-    // Do your stuff here (query API or SQL db, etc...)
-    
-    //console.log(req.body);
-    winston.info('[login] email is ' + req.query.email);
-    winston.info('[login] username is ' + username);
-    winston.info('[login] ready to start session login');
-    
-    var val = cookie.sign('2025', 'ruttabegga');
-    winston.info('signed is ' + val);
-    var val_encoded = new Buffer(val).toString('base64');
-    winston.info('encoded is ' + val_encoded);
-    var val_decoded = new Buffer(val_encoded,'base64').toString('ascii');
-    winston.info('decoded is ' + val_decoded);
-    var val2 = cookie.unsign(val_decoded, 'ruttabegga');
-    winston.info('unsigned is ' + val2);
-    
-    request('https://app.contentblvd.com/v1/users/' + req.body.id, function (err, response, body) {
-			if (err) {
-				return next(null, null);
-			}
-			if (response.statusCode === 200) {
-				var data = JSON.parse(body);
-				console.log(data);
-			}
-			next(null, user);
-		});
-    
-    
-    var uid = 1;
-    
-    if (uid) {
-      
-      next(null, {
-  		  uid: uid
-  	  }, '[[success:authentication-successful]]');
-  	  
-  	} else{  
+		Step 2: Give it a whirl. If you see the congrats message, you're doing well so far!
+
+		Step 3: Customise the `parseUserReturn` method to normalise your user route's data return into
+				a format accepted by NodeBB. Instructions are provided there. (Line 137)
+
+		Step 4: If all goes well, you'll be able to login/register via your OAuth endpoint credentials.
+	*/
+
+	var User = module.parent.require('./user'),
+		Groups = module.parent.require('./groups'),
+		meta = module.parent.require('./meta'),
+		passport = module.parent.require('passport'),
+		BearerStrategy = require('passport-http-bearer').Strategy,
+		fs = module.parent.require('fs'),
+		path = module.parent.require('path'),
+		nconf = module.parent.require('nconf'),
+		winston = module.parent.require('winston'),
+		async = module.parent.require('async'),
+		cookie = require('cookie-signature'),
+    request = require('request')
+
+		constants = Object.freeze({
+			name: 'contentblvd',	// Something unique to your OAuth provider in lowercase, like "github", or "nodebb"
+			userRoute: ''	// This is the address to your app's "user profile" API endpoint (expects JSON)
+		}),
+		CB = {}, passportOAuth, opts;
 	
-    	// But if the login was unsuccessful, pass an error back, like so:
-    	next(new Error('[[error:invalid-username-or-password]]'));
-    	
-    }
-};
 
-module.exports = plugin;
+	CB.getStrategy = function(strategies, callback) {
+    passport.use(new BearerStrategy({}, function(token, done) {
+  		// Find the user by token.  If there is no user with the given token, set
+  		// the user to `false` to indicate failure.  Otherwise, return the
+  		// authenticated `user`.  Note that in a production-ready application, one
+  		// would want to validate the token for authenticity.
+  		
+  		// do decodin, unsigning, fetch user here
+  		var token_decoded = new Buffer(token,'base64').toString('ascii');
+      winston.info('decoded is ' + token_decoded);
+      var CBid = cookie.unsign(token_decoded, 'ruttabegga');
+      winston.info('unsigned is ' + CBid);
+      
+      request('https://app.contentblvd.com/v1/users/' + CBid, function (err, response, body) {
+  			if (err) {
+  				return done(null, null);
+  			}
+  			if (response.statusCode === 200) {
+  				var data = JSON.parse(body);
+  				console.log(data);
+  				var profile = {};
+      		profile.id = data.id;
+      		profile.displayName = data.name;
+      		profile.emails = [{ value: data.email }];
+      		if (data.default_role == 'admin') {
+      		  profile.isAdmin = 1;
+      		}
+      		
+      		CB.login({
+    				CBid: profile.id,
+    				handle: profile.displayName,
+    				email: profile.email,
+    				isAdmin: profile.isAdmin
+    			}, function(err, user) {
+    				if (err) {
+    					return done(err);
+    				}
+    				done(null, user);
+    			});
+  			}
+  			
+  		});
+  		
+  	}));
+
+		strategies.push({
+			name: constants.name,
+			url: '/auth/' + constants.name,
+			callbackURL: '/auth/' + constants.name + '/callback',
+			icon: 'fa-check-square'
+		});
+
+		callback(null, strategies);
+
+	};
+
+	CB.login = function(payload, callback) {
+		OAuth.getUidByCBid(payload.CBid, function(err, uid) {
+			if(err) {
+				return callback(err);
+			}
+
+			if (uid !== null) {
+				// Existing User
+				callback(null, {
+					uid: uid
+				});
+			} else {
+				// New User
+				var success = function(uid) {
+					// Save provider-specific information to the user
+					User.setUserField(uid, constants.name + 'Id', payload.CBid);
+					db.setObjectField(constants.name + 'Id:uid', payload.CBid, uid);
+
+					if (payload.isAdmin) {
+						Groups.join('administrators', uid, function(err) {
+							callback(null, {
+								uid: uid
+							});
+						});
+					} else {
+						callback(null, {
+							uid: uid
+						});
+					}
+				};
+
+				User.getUidByEmail(payload.email, function(err, uid) {
+					if(err) {
+						return callback(err);
+					}
+
+					if (!uid) {
+						User.create({
+							username: payload.handle,
+							email: payload.email
+						}, function(err, uid) {
+							if(err) {
+								return callback(err);
+							}
+
+							success(uid);
+						});
+					} else {
+						success(uid); // Existing account -- merge
+					}
+				});
+			}
+		});
+	};
+
+	CB.getUidByCBid = function(oAuthid, callback) {
+		db.getObjectField(constants.name + 'Id:uid', CBid, function(err, uid) {
+			if (err) {
+				return callback(err);
+			}
+			callback(null, uid);
+		});
+	};
+
+	CB.deleteUserData = function(uid, callback) {
+		async.waterfall([
+			async.apply(User.getUserField, uid, constants.name + 'Id'),
+			function(CBIdToDelete, next) {
+				db.deleteObjectField(constants.name + 'Id:uid', CBIdToDelete, next);
+			}
+		], function(err) {
+			if (err) {
+				winston.error('[sso-cb] Could not remove CBId data for uid ' + uid + '. Error: ' + err);
+				return callback(err);
+			}
+			callback(null, uid);
+		});
+	};
+
+	module.exports = CB;
+}(module));
