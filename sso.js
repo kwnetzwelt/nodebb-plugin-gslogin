d64f13b140823da8c4742b3c37345bbe9ab31115
markcarey <mark@markcarey.com>
2015-09-29 12:48:01 -0400

    debug

--- sso.js
+++ sso.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
       }
     });
     
-    $("body").on('click','a', function(){
+    $("body").on("click","a", function(){
       console.log("clicked!");
       var target = $(this).attr('href');
       if (target == '/login') {
